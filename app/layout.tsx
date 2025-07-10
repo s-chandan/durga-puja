a852476f01f3ae200c6a2a245edcdeae15851d37
Chandan Sharma <csharma24744@gmail.com>
2025-07-10 16:01:42 +0530

    update metadata in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Jai Mata Di',
+  description: 'Jai Durga Maa',
+  generator: 'Chandan Sharma',
 }
 
 export default function RootLayout({
